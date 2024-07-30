1a7543078b68f5f7e0e42b1f05043ddcf8bdd3d3
AbhijeetBorole <aborole136@gmail.com>
2024-07-30 15:05:13 +0530

    Project started (created)

---
+++ MiniProj01/src/main/java/com/ab/MiniProj01Application.java
@@ -0,0 +1,13 @@
+package com.ab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProj01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProj01Application.class, args);
+	}
+
+}
